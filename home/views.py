20d5026a0fd5b27b746a05c8c52da51f0a97e0d9
asplesa <reillys2004@gmail.com>
2021-07-12 20:45:38 +0000

    Add featured products to homepage

--- home/views.py
+++ home/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
 
 # Create your views here.
+
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    featured_product_1 = get_object_or_404(Product, pk=1)
+    featured_product_2 = get_object_or_404(Product, pk=5)
+    featured_product_3 = get_object_or_404(Product, pk=10)
+
+    template = 'home/index.html'
+    context = {
+        'featured_product_1': featured_product_1,
+        'featured_product_2': featured_product_2,
+        'featured_product_3': featured_product_3,
+    }
+
+    return render(request, template, context)

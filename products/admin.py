9a30a955b68658b7b56e73f336455541130353c1
asplesa <reillys2004@gmail.com>
2021-07-13 10:15:07 +0000

    Fixed styling issues

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Product, Category
 
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
@@ -12,6 +13,7 @@ class ProductAdmin(admin.ModelAdmin):
     )
 
     ordering = ('sku',)
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (

ce4e8a17d36937a93950c00afc37fb45964a1b1d
asplesa <reillys2004@gmail.com>
2021-07-14 13:48:56 +0000

    Removed secret key and det debug

--- ellie_face/settings.py
+++ ellie_face/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tu@%!&uguviaf_02%q*3r(437ly^xf7y%tp6ex0^u!*v#yjl+h'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ellie-face.herokuapp.com', 'localhost']
 
